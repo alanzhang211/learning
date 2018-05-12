14ae2c87399812220699940379e2c410145de52c
alanzhang211 <alanzhang211@outlook.com>
2018-05-12 13:39:02 +0800

    格式代码

--- juc/cyclicBarrier/CyclicBarrierDemo.java
+++ juc/cyclicBarrier/CyclicBarrierDemo.java
@@ -5,12 +5,11 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- *
  * Created by alanzhang211 on 2018/5/12.
  */
 public class CyclicBarrierDemo {
     //4位运动员，都到达终点，结束
-    static CyclicBarrier cyclicBarrier = new CyclicBarrier(4,new FinishPoint());
+    static CyclicBarrier cyclicBarrier = new CyclicBarrier(4, new FinishPoint());
 
     static class FinishPoint implements Runnable {
         @Override
@@ -20,12 +19,12 @@ public class CyclicBarrierDemo {
     }
 
     public static void main(String[] args) {
-        List<Runner> runners  = new ArrayList<Runner>(4);
-        for (int i=0 ;i < 4 ; i++){
-            runners.add(new Runner(cyclicBarrier,"runner"+"-"+i));
+        List<Runner> runners = new ArrayList<Runner>(4);
+        for (int i = 0; i < 4; i++) {
+            runners.add(new Runner(cyclicBarrier, "runner" + "-" + i));
         }
         Executor executor = Executors.newFixedThreadPool(5);
-        for (Runner runner:runners) {
+        for (Runner runner : runners) {
             executor.execute(runner);
         }
     }
--- juc/cyclicBarrier/Runner.java
+++ juc/cyclicBarrier/Runner.java
@@ -4,21 +4,21 @@ import java.util.concurrent.CyclicBarrier;
  * 运动员实现类
  * Created by alanzhang211 on 2018/5/12.
  */
-public class Runner implements Runnable{
+public class Runner implements Runnable {
     private CyclicBarrier cyclicBarrier;
     private String name;
 
     public Runner(CyclicBarrier cyclicBarrier, String name) {
-            this.cyclicBarrier = cyclicBarrier;
-            this.name = name;
-        }
+        this.cyclicBarrier = cyclicBarrier;
+        this.name = name;
+    }
 
-        @Override
-        public void run() {
-            System.out.println("运动员：" + name + " 到达终点！");
-            try {
-                cyclicBarrier.await();
-            } catch (Exception e) {
-            }
+    @Override
+    public void run() {
+        System.out.println("运动员：" + name + " 到达终点！");
+        try {
+            cyclicBarrier.await();
+        } catch (Exception e) {
         }
     }
+}
