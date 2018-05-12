0788cafaa94d7df0cc51efc625988530254d7b73
alanzhang211 <alanzhang211@outlook.com>
2018-05-12 13:36:29 +0800

    CyclicBarrier demo

---
+++ juc/cyclicBarrier/CyclicBarrierDemo.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * Created by alanzhang211 on 2018/5/12.
+ */
+public class CyclicBarrierDemo {
+    //4位运动员，都到达终点，结束
+    static CyclicBarrier cyclicBarrier = new CyclicBarrier(4,new FinishPoint());
+
+    static class FinishPoint implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("全部到达终点！");
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Runner> runners  = new ArrayList<Runner>(4);
+        for (int i=0 ;i < 4 ; i++){
+            runners.add(new Runner(cyclicBarrier,"runner"+"-"+i));
+        }
+        Executor executor = Executors.newFixedThreadPool(5);
+        for (Runner runner:runners) {
+            executor.execute(runner);
+        }
+    }
+}
---
+++ juc/cyclicBarrier/Runner.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 运动员实现类
+ * Created by alanzhang211 on 2018/5/12.
+ */
+public class Runner implements Runnable{
+    private CyclicBarrier cyclicBarrier;
+    private String name;
+
+    public Runner(CyclicBarrier cyclicBarrier, String name) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("运动员：" + name + " 到达终点！");
+            try {
+                cyclicBarrier.await();
+            } catch (Exception e) {
+            }
+        }
+    }
